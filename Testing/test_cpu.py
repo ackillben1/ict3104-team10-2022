e5c8721d7b4896e540ccabc29ef6a72f9e561e66
ackillben1 <angyimin98@gmail.com>
2022-11-11 17:25:27 +0800

    Update test_cpu.py
    Changes made same as cuda test file.

--- Testing/test_cpu.py
+++ Testing/test_cpu.py
@@ -201,14 +201,14 @@ def run_network(model, data, gpu, epoch=0, baseline=False):
 def save_list_to_csv(list, vid_name):
     fields = ['captions', 'start_frame', 'end_frame']
 
-    with open('./captions/caption_' + vid_name + '.csv', 'w') as data_file:
+    with open('./Data_Folder/Captions/caption_' + vid_name + '.csv', 'w') as data_file:
         data_file.truncate()
         csv_writer = csv.writer(data_file)
         csv_writer.writerow(fields)
         csv_writer.writerows(list)
 
-    df = pd.read_csv('./captions/caption_' + vid_name + '.csv')
-    df.to_csv('./captions/caption_' + vid_name + '.csv', index=False)
+    df = pd.read_csv('./Data_Folder/Captions/caption_' + vid_name + '.csv')
+    df.to_csv('./Data_Folder/Captions/caption_' + vid_name + '.csv', index=False)
 
 
 def val_step(model, gpu, dataloader, classes):
@@ -241,8 +241,8 @@ def val_step(model, gpu, dataloader, classes):
         events = []
 
         for event in predicted_event:
-            start = current + 0.1
-            end = start + no_frame
+            start = round(current)
+            end = start + round(no_frame)
             current = end
             current_event = event_list[event]
             events.append([current_event, start, end])
