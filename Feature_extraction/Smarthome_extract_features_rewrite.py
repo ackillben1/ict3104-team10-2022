d2b32ea51d6df80c6ea7c06a8b17a13581e88452
Jaspzx <jaspzx@hotmail.com>
2022-11-21 09:59:05 +0800

    Push Demo Codes

--- Feature_extraction/Smarthome_extract_features_rewrite.py
+++ Feature_extraction/Smarthome_extract_features_rewrite.py
@@ -4,14 +4,19 @@ import argparse
 from multiprocessing import cpu_count
 import torch
 import torch._utils
+
 try:
     torch._utils._rebuild_tensor_v2
 except AttributeError:
-    def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
+
+    def _rebuild_tensor_v2(
+        storage, storage_offset, size, stride, requires_grad, backward_hooks
+    ):
         tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
         tensor.requires_grad = requires_grad
         tensor._backward_hooks = backward_hooks
         return tensor
+
     torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
 import torch.nn as nn
 import torch.nn.functional as F
@@ -26,13 +31,13 @@ from tqdm import tqdm
 import cv2
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-mode', type=str, help='rgb or flow')
-parser.add_argument('-load_model', type=str)
-parser.add_argument('-root', type=str)
-parser.add_argument('-gpu', type=str)
-parser.add_argument('-save_dir', type=str)
-parser.add_argument('-split', type=str)
-parser.add_argument('-window_size', type=int)
+parser.add_argument("-mode", type=str, help="rgb or flow")
+parser.add_argument("-load_model", type=str)
+parser.add_argument("-root", type=str)
+parser.add_argument("-gpu", type=str)
+parser.add_argument("-save_dir", type=str)
+parser.add_argument("-split", type=str)
+parser.add_argument("-window_size", type=int)
 args = parser.parse_args()
 
 
@@ -60,16 +65,16 @@ def make_dataset(vid_name, root):
             break
         w, h, c = img.shape
         if w < 224 or h < 224:
-            d = 226. - min(w, h)
+            d = 226.0 - min(w, h)
             sc = 1 + d / min(w, h)
             img = cv2.resize(img, dsize=(0, 0), fx=sc, fy=sc)
-        img = (img / 255.) * 2 - 1
+        img = (img / 255.0) * 2 - 1
         frames.append(img)
         success, img = vidCap.read()
         if success:
             count += 1
 
-    print(f'Read {count} frames')
+    print(f"Read {count} frames")
     frames = np.asarray(frames, dtype=np.float32)
     frames = frames[np.newaxis, :, :, :, :]
 
@@ -80,14 +85,14 @@ def video_to_tensor(pic):
     return torch.from_numpy(pic.transpose([0, 4, 1, 2, 3]))
 
 
-def run(mode='rgb', root='', vid_name='', batch_size=1, load_model='', save_dir=''):
+def run(mode="rgb", root="", vid_name="", batch_size=1, load_model="", save_dir=""):
     # print(root)
     # if os.path.exists(os.path.join(save_dir, vid_name + '.npy')):
     #    print('feature exist!')
     #    exit()
 
     # setup the model
-    if mode == 'flow':
+    if mode == "flow":
         i3d = InceptionI3d(400, in_channels=2)
     else:
         i3d = InceptionI3d(48, in_channels=3)
@@ -96,52 +101,63 @@ def run(mode='rgb', root='', vid_name='', batch_size=1, load_model='', save_dir=
     i3d = i3d.cuda()
     i3d_parallel = torch.nn.DataParallel(i3d)
 
-    frameCount, frames = get_frames(vid_name + '.mp4', root)
-    print(f'Frame Count: {frameCount+1}')
+    frameCount, frames = get_frames(vid_name + ".mp4", root)
+    print(f"Frame Count: {frameCount+1}")
 
     features = []
     np_frames = []
 
     i3d_parallel.train(False)
-    #if os.path.exists(os.path.join(save_dir, vid_name + '.npy')):
+    # if os.path.exists(os.path.join(save_dir, vid_name + '.npy')):
     #    exit()
 
-    print('Processing frame(s)')
+    print("Processing frame(s)")
     for count in tqdm(range(frameCount)):
 
-        if count % 16 != 0 or count == 0:
+        if count % 10 != 0 or count == 0:
 
             img = frames[count]
             w, h, c = img.shape
             if w < 224 or h < 22:
-                d = 226. - min(w, h)
+                d = 226.0 - min(w, h)
                 sc = 1 + d / min(w, h)
                 img = cv2.resize(img, dsize=(0, 0), fx=sc, fy=sc)
-            img = (img / 255.) * 2 - 1
+            img = (img / 255.0) * 2 - 1
             np_frames.append(img)
             count += 1
         else:
-            np_frames = video_to_tensor(np.asarray(np_frames, dtype=np.float32)[np.newaxis, :, :, :, :])
+            np_frames = video_to_tensor(
+                np.asarray(np_frames, dtype=np.float32)[np.newaxis, :, :, :, :]
+            )
             np_frames_processed = Variable(np_frames.cuda())
             temp_features = i3d_parallel.module.extract_features(np_frames_processed)
-            features.append(temp_features.squeeze(0).permute(1, 2, 3, 0).data.cpu().numpy())
+            features.append(
+                temp_features.squeeze(0).permute(1, 2, 3, 0).data.cpu().numpy()
+            )
             np_frames = []
 
             img = frames[count]
             w, h, c = img.shape
             if w < 224 or h < 224:
-                d = 226. - min(w, h)
+                d = 226.0 - min(w, h)
                 sc = 1 + d / min(w, h)
                 img = cv2.resize(img, dsize=(0, 0), fx=sc, fy=sc)
-            img = (img / 255.) * 2 - 1
+            img = (img / 255.0) * 2 - 1
             np_frames.append(img)
             count += 1
 
     np.save(os.path.join(save_dir, vid_name), features)
-    print('save_path: ', os.path.join(save_dir, vid_name)+'.npy')
+    print("save_path: ", os.path.join(save_dir, vid_name) + ".npy")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # need to add argparse
-    print('cuda_avail', torch.cuda.is_available())
+    print("cuda_avail", torch.cuda.is_available())
     torch.cuda.empty_cache()
-    run(mode=args.mode, root=args.root, load_model=args.load_model, save_dir=args.save_dir, vid_name=args.split)
+    run(
+        mode=args.mode,
+        root=args.root,
+        load_model=args.load_model,
+        save_dir=args.save_dir,
+        vid_name=args.split,
+    )
--- Testing/test.py
+++ Testing/test.py
@@ -16,36 +16,36 @@ import re
 
 
 def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+        raise argparse.ArgumentTypeError("Boolean value expected.")
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-mode', type=str, help='rgb or flow (or joint for eval)')
-parser.add_argument('-test', type=str2bool, default='True', help='train or eval')
-parser.add_argument('-comp_info', type=str)
-parser.add_argument('-rgb_model_file', type=str)
-parser.add_argument('-flow_model_file', type=str)
-parser.add_argument('-gpu', type=str, default='4')
-parser.add_argument('-dataset', type=str, default='charades')
-parser.add_argument('-rgb_root', type=str, default='no_root')
-parser.add_argument('-flow_root', type=str, default='no_root')
-parser.add_argument('-type', type=str, default='original')
-parser.add_argument('-lr', type=str, default='0.1')
-parser.add_argument('-epoch', type=str, default='50')
-parser.add_argument('-model', type=str, default='')
-parser.add_argument('-APtype', type=str, default='wap')
-parser.add_argument('-randomseed', type=str, default='False')
-parser.add_argument('-load_model', type=str, default='False')
-parser.add_argument('-num_channel', type=str, default='False')
-parser.add_argument('-batch_size', type=str, default='False')
-parser.add_argument('-kernelsize', type=str, default='False')
-parser.add_argument('-feat', type=str, default='False')
-parser.add_argument('-split_setting', type=str, default='CS')
+parser.add_argument("-mode", type=str, help="rgb or flow (or joint for eval)")
+parser.add_argument("-test", type=str2bool, default="True", help="train or eval")
+parser.add_argument("-comp_info", type=str)
+parser.add_argument("-rgb_model_file", type=str)
+parser.add_argument("-flow_model_file", type=str)
+parser.add_argument("-gpu", type=str, default="4")
+parser.add_argument("-dataset", type=str, default="charades")
+parser.add_argument("-rgb_root", type=str, default="no_root")
+parser.add_argument("-flow_root", type=str, default="no_root")
+parser.add_argument("-type", type=str, default="original")
+parser.add_argument("-lr", type=str, default="0.1")
+parser.add_argument("-epoch", type=str, default="50")
+parser.add_argument("-model", type=str, default="")
+parser.add_argument("-APtype", type=str, default="wap")
+parser.add_argument("-randomseed", type=str, default="False")
+parser.add_argument("-load_model", type=str, default="False")
+parser.add_argument("-num_channel", type=str, default="False")
+parser.add_argument("-batch_size", type=str, default="False")
+parser.add_argument("-kernelsize", type=str, default="False")
+parser.add_argument("-feat", type=str, default="False")
+parser.add_argument("-split_setting", type=str, default="CS")
 args = parser.parse_args()
 
 import torch
@@ -84,12 +84,12 @@ import math
 # dirpath = os.getcwd()
 # print("current directory is : " + dirpath)
 
-if str(args.APtype) == 'map':
+if str(args.APtype) == "map":
     from apmeter import APMeter
 
 batch_size = int(args.batch_size)
 
-if args.dataset == 'TSU':
+if args.dataset == "TSU":
     split_setting = str(args.split_setting)
 
     from Testing.smarthome_i3d_per_video import TSU as Dataset
@@ -97,17 +97,19 @@ if args.dataset == 'TSU':
 
     classes = 51
 
-    if split_setting == 'CS':
-        test_split = './Testing/data/smarthome_CS_51.json'
+    if split_setting == "CS":
+        test_split = "./Testing/data/smarthome_CS_51.json"
 
-    elif split_setting == 'CV':
-        test_split = './Testing/data/smarthome_CV_51.json'
+    elif split_setting == "CV":
+        test_split = "./Testing/data/smarthome_CV_51.json"
 
     # rgb_root = 'C:/Users/angyi/Documents/SIT Year 3/3104/Project/TSU_RGB_i3d_feat/RGB_i3d_16frames_64000_SSD'
-    rgb_root = "C:/Users/angyi/Documents/SIT Year 3/3104/Project/TSU_RGB_i3d_feat/RGB_i3d_16frames_64000_SSD"
+    # rgb_root = "C:/Users/angyi/Documents/SIT Year 3/3104/Project/TSU_RGB_i3d_feat/RGB_i3d_16frames_64000_SSD"
+    rgb_root = "C:/Users/jaspz/Documents/GitHub/ict3104-team10-2022/Training/RGB_i3d_16frames_64000_SSD"
+
     # skeleton_root='C:/Users/angyi/Documents/SIT Year 3/3104/Project/TSU_3DPose_AGCN_feat/2sAGCN_16frames_64000'
     # rgb_root = './Testing/RGB_i3d_16frames_64000_SSD'
-    skeleton_root = './Testing/TSU_3DPose_AGCN_feat/2sAGCN_16frames_64000'
+    skeleton_root = "./Testing/TSU_3DPose_AGCN_feat/2sAGCN_16frames_64000"
 
 
 def sigmoid(x):
@@ -117,27 +119,40 @@ def sigmoid(x):
 def load_data_rgb_skeleton(val_split, root_skeleton, root_rgb):
     # Load Data
 
-    val_dataset = Dataset(val_split, 'testing', root_skeleton, root_rgb, batch_size, classes)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=0,
-                                                 pin_memory=True, collate_fn=collate_fn)  # 2
+    val_dataset = Dataset(
+        val_split, "testing", root_skeleton, root_rgb, batch_size, classes
+    )
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=0,
+        pin_memory=True,
+        collate_fn=collate_fn,
+    )  # 2
 
-    dataloaders = {'val': val_dataloader}
-    datasets = {'val': val_dataset}
+    dataloaders = {"val": val_dataloader}
+    datasets = {"val": val_dataset}
     return dataloaders, datasets
-
 
 
 def load_data(val_split, root):
     # Load Data
     # print(val_split, root, batch_size, classes)
 
-    val_dataset = Dataset(val_split, 'testing', root, batch_size, classes)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=True, num_workers=2,
-                                                 pin_memory=True, collate_fn=collate_fn)
+    val_dataset = Dataset(val_split, "testing", root, batch_size, classes)
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=1,
+        shuffle=True,
+        num_workers=2,
+        pin_memory=True,
+        collate_fn=collate_fn,
+    )
     val_dataloader.root = root
 
-    dataloaders = {'val': val_dataloader}
-    datasets = {'val': val_dataset}
+    dataloaders = {"val": val_dataloader}
+    datasets = {"val": val_dataset}
     return dataloaders, datasets
 
 
@@ -145,7 +160,7 @@ def load_labels():
     with open("./Testing/data/all_labels.txt") as file_in:
         lines = []
         for line in file_in:
-            line = re.sub(r'\d+', '', line)
+            line = re.sub(r"\d+", "", line)
             line = line.strip()
             lines.append(line)
         return lines
@@ -157,7 +172,9 @@ def run(models, criterion, num_classes):
 
     probs = []
     for model, gpu, dataloader, optimizer, sched, model_file in models:
-        prob_val, val_loss, val_map = val_step(model, gpu, dataloader['val'], num_classes)
+        prob_val, val_loss, val_map = val_step(
+            model, gpu, dataloader["val"], num_classes
+        )
         probs.append(prob_val)
         sched.step(val_loss)
 
@@ -174,7 +191,7 @@ def run_network(model, data, gpu, epoch=0, baseline=False):
     mask_list = torch.sum(mask, 1)
     mask_new = np.zeros((mask.size()[0], classes, mask.size()[1]))
     for i in range(mask.size()[0]):
-        mask_new[i, :, :int(mask_list[i])] = np.ones((classes, int(mask_list[i])))
+        mask_new[i, :, : int(mask_list[i])] = np.ones((classes, int(mask_list[i])))
     mask_new = torch.from_numpy(mask_new).float()
     mask_new = Variable(mask_new.cuda(gpu))
 
@@ -189,7 +206,9 @@ def run_network(model, data, gpu, epoch=0, baseline=False):
     # print('outputs_final',outputs_final.size())
     outputs_final = outputs_final.permute(0, 2, 1)
     probs_f = F.sigmoid(outputs_final) * mask.unsqueeze(2)
-    loss_f = F.binary_cross_entropy_with_logits(outputs_final, labels, size_average=False)
+    loss_f = F.binary_cross_entropy_with_logits(
+        outputs_final, labels, size_average=False
+    )
     loss_f = torch.sum(loss_f) / torch.sum(mask)
 
     loss = loss_f
@@ -201,16 +220,16 @@ def run_network(model, data, gpu, epoch=0, baseline=False):
 
 
 def save_list_to_csv(list, vid_name):
-    fields = ['captions', 'start_frame', 'end_frame']
+    fields = ["captions", "start_frame", "end_frame"]
 
-    with open('./Data_Folder/Captions/caption_' + vid_name + '.csv', 'w') as data_file:
+    with open("./Data_Folder/Captions/caption_" + vid_name + ".csv", "w") as data_file:
         data_file.truncate()
         csv_writer = csv.writer(data_file)
         csv_writer.writerow(fields)
         csv_writer.writerows(list)
 
-    df = pd.read_csv('./Data_Folder/Captions/caption_' + vid_name + '.csv')
-    df.to_csv('./Data_Folder/Captions/caption_' + vid_name + '.csv', index=False)
+    df = pd.read_csv("./Data_Folder/Captions/caption_" + vid_name + ".csv")
+    df.to_csv("./Data_Folder/Captions/caption_" + vid_name + ".csv", index=False)
 
 
 def val_step(model, gpu, dataloader, classes):
@@ -252,10 +271,18 @@ def val_step(model, gpu, dataloader, classes):
             end = start + round(no_frame)
             current = end
             current_event = event_list[event]
-            events.append([current_event, start, end, vid_name, (100 * probs.data.cpu().numpy()[0][num_iter][event])])
+            events.append(
+                [
+                    current_event,
+                    start,
+                    end,
+                    vid_name,
+                    (100 * probs.data.cpu().numpy()[0][num_iter][event]),
+                ]
+            )
 
         # List of action predicted, start, end, vid name, prob
-        save_list_to_csv(events)
+        save_list_to_csv(events, vid_name)
 
         error += err.data
         tot_loss += loss.data
@@ -269,38 +296,40 @@ def val_step(model, gpu, dataloader, classes):
     epoch_loss = tot_loss / num_iter
 
     val_map = torch.sum(100 * apm.value()) / torch.nonzero(100 * apm.value()).size()[0]
-    print('val-map:', val_map)
+    print("val-map:", val_map)
     print(100 * apm.value())
 
     # Write predictions to CSV file
-    fields = ['Activity_Index', 'Average Class Prediction']
+    fields = ["Activity_Index", "Average Class Prediction"]
     rows = []
     init_flag = False
-    tempApm = (100*apm.value()).tolist()
+    tempApm = (100 * apm.value()).tolist()
     index = 0
     for value in tempApm:
         rows.append([index, value])
         index = index + 1
-            
-    with open("./Testing/results/prob_values.csv", 'w', newline='') as csv_file:
+
+    with open("./Testing/results/prob_values.csv", "w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(fields)
         csv_writer.writerows(rows)
-        
-    # Combine activity names and predictions to one CSV file    
-    activity_names = pd.read_csv(r'./Testing/data/all_labels.csv')
-    prob_values =  pd.read_csv(r'./Testing/results/prob_values.csv')
-    merge_output = pd.merge(activity_names, prob_values,on='Activity_Index',how='outer')
-    final_output = merge_output.drop(columns=['Activity_Index'])
-    final_output.to_csv('./Testing/results/results.csv', index=False)
-    
+
+    # Combine activity names and predictions to one CSV file
+    activity_names = pd.read_csv(r"./Testing/data/all_labels.csv")
+    prob_values = pd.read_csv(r"./Testing/results/prob_values.csv")
+    merge_output = pd.merge(
+        activity_names, prob_values, on="Activity_Index", how="outer"
+    )
+    final_output = merge_output.drop(columns=["Activity_Index"])
+    final_output.to_csv("./Testing/results/results.csv", index=False)
+
     # Append val_map, epoch_loss, total_value and events CSV file
-    val_map_value = ['Val Map',float(val_map)]
-    epoch_loss_value = ['Epoch Loss',float(epoch_loss)]
-    total_loss_value = ['Total Loss', float(tot_loss)]
-    
-    fields2= ["Event", "Start_frame", "End_frame", "Video_name", "Prediction Accuracy"]
-    with open('./Testing/results/results.csv','a',newline='') as fd:
+    val_map_value = ["Val Map", float(val_map)]
+    epoch_loss_value = ["Epoch Loss", float(epoch_loss)]
+    total_loss_value = ["Total Loss", float(tot_loss)]
+
+    fields2 = ["Event", "Start_frame", "End_frame", "Video_name", "Prediction Accuracy"]
+    with open("./Testing/results/results.csv", "a", newline="") as fd:
         csv_writer = csv.writer(fd)
         csv_writer.writerow("")
         csv_writer.writerow(val_map_value)
@@ -309,46 +338,81 @@ def val_step(model, gpu, dataloader, classes):
         csv_writer.writerow("")
         csv_writer.writerow(fields2)
         csv_writer.writerows(events)
-        
-    print('\033[92m'+"\033[1m"+ "==============================================================="+ "\033[0m"+'\033[0m')
-    print(Back.GREEN+"\033[1m"+ u'\u2713'+ " results exported to: ./Testing/results/results.csv"+ "\033[0m")
-    
+
+    print(
+        "\033[92m"
+        + "\033[1m"
+        + "==============================================================="
+        + "\033[0m"
+        + "\033[0m"
+    )
+    print(
+        Back.GREEN
+        + "\033[1m"
+        + "\u2713"
+        + " results exported to: ./Testing/results/results.csv"
+        + "\033[0m"
+    )
+
     # Generate Graphs on wandb
     # Generate table
-    table = wandb.Table(data=final_output,columns=["Activity", "Average Class Prediction"] )
-    
+    table = wandb.Table(
+        data=final_output, columns=["Activity", "Average Class Prediction"]
+    )
+
     # Plot graphs
-    wandb.log({"Average Class Prediction":wandb.plot.bar(table, "Activity", "Average Class Prediction")})
-    wandb.log({"Epoch Loss": epoch_loss,
-               "Total Loss": tot_loss,
-               "val_map": val_map,
-        })
-     
-    print('\033[92m'+"\033[1m"+ "==============================================================="+ "\033[0m"+'\033[0m')
-    print(Back.GREEN+"\033[1m"+ u'\u2713'+ " Graphs generated successfully!" + "\033[0m")
+    wandb.log(
+        {
+            "Average Class Prediction": wandb.plot.bar(
+                table, "Activity", "Average Class Prediction"
+            )
+        }
+    )
+    wandb.log(
+        {
+            "Epoch Loss": epoch_loss,
+            "Total Loss": tot_loss,
+            "val_map": val_map,
+        }
+    )
+
+    print(
+        "\033[92m"
+        + "\033[1m"
+        + "==============================================================="
+        + "\033[0m"
+        + "\033[0m"
+    )
+    print(
+        Back.GREEN
+        + "\033[1m"
+        + "\u2713"
+        + " Graphs generated successfully!"
+        + "\033[0m"
+    )
 
     apm.reset()
     return full_probs, epoch_loss, val_map
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # print(str(args.model))
     # print('batch_size:', batch_size)
     # print('cuda_avail', torch.cuda.is_available())
 
-    if args.mode == 'flow':
-        print('flow mode')
+    if args.mode == "flow":
+        print("flow mode")
         dataloaders, datasets = load_data(test_split, flow_root)
-    elif args.mode == 'skeleton':
-        print('Pose mode')
+    elif args.mode == "skeleton":
+        print("Pose mode")
         dataloaders, datasets = load_data(test_split, skeleton_root)
-    elif args.mode == 'rgb':
-        print('RGB mode')
+    elif args.mode == "rgb":
+        print("RGB mode")
         dataloaders, datasets = load_data(test_split, rgb_root)
 
     if args.test:
         num_channel = args.num_channel
-        if args.mode == 'skeleton':
+        if args.mode == "skeleton":
             input_channnel = 256
         else:
             input_channnel = 1024
@@ -360,7 +424,13 @@ if __name__ == '__main__':
             print("you are processing PDAN")
             from models import PDAN as Net
 
-            model = Net(num_stages=1, num_layers=5, num_f_maps=mid_channel, dim=input_channnel, num_classes=classes)
+            model = Net(
+                num_stages=1,
+                num_layers=5,
+                num_f_maps=mid_channel,
+                dim=input_channnel,
+                num_classes=classes,
+            )
 
         model = torch.nn.DataParallel(model)
 
@@ -372,7 +442,9 @@ if __name__ == '__main__':
             # model.load_state_dict(torch.load(str(args.load_model)))
             print("loaded", args.load_model)
 
-        pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        pytorch_total_params = sum(
+            p.numel() for p in model.parameters() if p.requires_grad
+        )
         # print('pytorch_total_params', pytorch_total_params)
         # print('num_channel:', num_channel, 'input_channnel:', input_channnel, 'num_classes:', num_classes)
         model.cuda()
@@ -381,5 +453,11 @@ if __name__ == '__main__':
         lr = float(args.lr)
         # print(lr)
         optimizer = optim.Adam(model.parameters(), lr=lr)
-        lr_sched = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=8, verbose=True)
-        run([(model, 0, dataloaders, optimizer, lr_sched, args.comp_info)], criterion, num_classes=classes)
+        lr_sched = optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, factor=0.5, patience=8, verbose=True
+        )
+        run(
+            [(model, 0, dataloaders, optimizer, lr_sched, args.comp_info)],
+            criterion,
+            num_classes=classes,
+        )
